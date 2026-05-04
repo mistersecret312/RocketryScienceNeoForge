cbab758f6cc211b5aecbc41fee75107026c2b65b
mistersecret312 <sergiigamer7@gmail.com>
2026-05-04 22:56:42 +0300

    error begone

--- src/main/java/net/mistersecret312/rocketry_science/RocketryScience.java
+++ src/main/java/net/mistersecret312/rocketry_science/RocketryScience.java
@@ -9,7 +9,6 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 @Mod(RocketryScience.MODID)
@@ -21,8 +20,6 @@ public class RocketryScience
 	{
 		modEventBus.addListener(this::commonSetup);
 		NeoForge.EVENT_BUS.register(this);
-
-		modEventBus.addListener(this::addCreative);
 	}
 
 	private void commonSetup(final FMLCommonSetupEvent event)
