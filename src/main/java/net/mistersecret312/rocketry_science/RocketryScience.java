a1d040cf739bac9a1705872ec13a2d0b828d5756
mistersecret312 <sergiigamer7@gmail.com>
2026-05-04 22:54:23 +0300

    Initial Commit

---
+++ src/main/java/net/mistersecret312/rocketry_science/RocketryScience.java
@@ -0,0 +1,47 @@
+package net.mistersecret312.rocketry_science;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(RocketryScience.MODID)
+public class RocketryScience
+{
+	public static final String MODID = "rocketry_science";
+
+	public RocketryScience(IEventBus modEventBus, ModContainer modContainer)
+	{
+		modEventBus.addListener(this::commonSetup);
+		NeoForge.EVENT_BUS.register(this);
+
+		modEventBus.addListener(this::addCreative);
+	}
+
+	private void commonSetup(final FMLCommonSetupEvent event)
+	{
+
+	}
+
+	@SubscribeEvent
+	public void onServerStarting(ServerStartingEvent event)
+	{
+	}
+
+	@EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ClientModEvents
+	{
+		@SubscribeEvent
+		public static void onClientSetup(FMLClientSetupEvent event)
+		{
+
+		}
+	}
+}
